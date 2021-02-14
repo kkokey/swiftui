a323e10e6c34e827e98a904ee86466ad6a723efd
namedboy <kkokey@gmail.com>
2021-02-15 00:20:20 +0900

    Initial Commit

---
+++ appdongnae/appdongnaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  appdongnaeApp.swift
+//  appdongnae
+//
+//  Created by aronkim on 2021/02/15.
+//
+
+import SwiftUI
+
+@main
+struct appdongnaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
